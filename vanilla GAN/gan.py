cfde89b54b8a50ec9660f92c3b4bcae3e85e5cf4
textstorm <jiangzhewise@gmail.com>
2017-11-25 09:19:26 +0800

    gan update

--- vanilla GAN/gan.py
+++ vanilla GAN/gan.py
@@ -14,42 +14,37 @@ def bias_variable(shape, name, initializer=None):
   return tf.get_variable(shape=shape, initializer=initializer, name=name)
 
 class GAN(object):
-  def __init__(self, args, session):
+  def __init__(self, args, sess, name='gan'):
     self.input_dim = args.input_dim
     self.noise_dim = args.noise_dim
-    self.g_h1_dim = args.g_h1_dim
-    self.g_h2_dim = args.g_h2_dim
-    self.d_h1_dim = args.d_h1_dim
-    self.d_h2_dim = args.d_h2_dim
+    self.sess = sess
     self.max_grad_norm = args.max_grad_norm
-    self.sess = session
+    self.learning_rate = args.learning_rate
 
-    self._build_placeholder()
+    self.add_placeholder()
 
-    self.optimizer = tf.train.AdamOptimizer(self.lr)
-    self.global_step = tf.get_variable('global_step', [], 'int32', tf.constant_initializer(0), trainable=False)
+    self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+    self.global_step = tf.Variable(0, trainable=False)
     
-    self._build_forward()
+    self._build_graph()
     self._build_loss()
     self._build_train()
 
     init_op = tf.global_variables_initializer()
     self.sess.run(init_op)
 
-  def _build_placeholder(self):
+  def add_placeholder(self):
     with tf.name_scope("Data"):
       self.x_images = tf.placeholder(tf.float32, [None, self.input_dim])
       self.noise = tf.placeholder(tf.float32, [None, self.noise_dim])
-    with tf.name_scope("Lr"):
-      self.lr = tf.placeholder(tf.float32, [], name="learning_rate")
 
   def _build_forward(self):
-    with tf.name_scope("Model"):
-      with tf.variable_scope("Generator"):
+    with tf.name_scope("model"):
+      with tf.variable_scope("generator"):
         self.G, self.g_var_list = self.generator(self.noise)
-      with tf.variable_scope("Discrininator"):
+      with tf.variable_scope("discrininator"):
         self.D_real, self.d_var_list = self.discriminator(self.x_images)
-      with tf.variable_scope("Discrininator", reuse=True):
+      with tf.variable_scope("discrininator", reuse=True):
         self.D_fake, _ = self.discriminator(self.G)
   
   def _build_loss(self):
@@ -59,13 +54,13 @@ class GAN(object):
       D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
                                     logits=self.D_fake, labels=tf.zeros_like(self.D_fake)))
       G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-                                    logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
+                              logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
 
       self.D_loss =  (D_loss_real + D_loss_fake)
       self.G_loss =  G_loss
 
   def _build_train(self):
-    with tf.name_scope("Train"):
+    with tf.name_scope("train"):
       d_grads_and_vars = self.optimizer.compute_gradients(self.D_loss, var_list=self.d_var_list)
       d_grads_and_vars = [(tf.clip_by_norm(g, self.max_grad_norm), v) for g, v in d_grads_and_vars]
       g_grads_and_vars = self.optimizer.compute_gradients(self.G_loss, var_list=self.g_var_list)
@@ -73,23 +68,15 @@ class GAN(object):
       self.train_op_d = self.optimizer.apply_gradients(d_grads_and_vars, global_step=self.global_step)
       self.train_op_g = self.optimizer.apply_gradients(g_grads_and_vars, global_step=self.global_step)
 
-  def d_batch_fit(self, x_images, noise, lr):
-    feed_dict = {}
-    feed_dict[self.x_images] = x_images
-    feed_dict[self.noise] = noise
-    feed_dict[self.lr] = lr
-    
+  def d_batch_fit(self, x_images, noise):
+    feed_dict = {self.x_images: x_images, self.noise:noise}
     loss, _ = self.sess.run([self.D_loss, self.train_op_d], feed_dict=feed_dict)
-
     return loss
 
-  def g_batch_fit(self, noise, lr):
-    feed_dict = {}
+  def g_batch_fit(self, noise):
+    feed_dict = {self.noise: noise}
     feed_dict[self.noise] = noise
-    feed_dict[self.lr] = lr
-
     loss, _ = self.sess.run([self.G_loss, self.train_op_g], feed_dict=feed_dict)
-
     return loss
 
   def generator(self, noise):
@@ -117,5 +104,4 @@ class GAN(object):
 
   def generate(self, noise):
     feed_dict = {self.noise: noise}
-
     return self.sess.run(self.G, feed_dict=feed_dict)
